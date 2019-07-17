802b8eac161d3345eb1f85d6f86c79f993fb5b14
hanati <hanati@tezign.com>
2019-07-17 18:46:38 +0800

    change pretrained model to 'resnet_v1_50_center_loss_3.ckpt'

--- trainer/train_softmax.py
+++ trainer/train_softmax.py
@@ -113,7 +113,7 @@ def trainer(train_img_root,
 
     net = ClassificationNet(sess=sess,
                             backbones='resnet_v1_50',
-                            pretrained_model='weight/pretrained/model-20190717-030305.ckpt-17902',
+                            pretrained_model='weight/pretrained/resnet_v1_50_center_loss_3.ckpt',
                             width=width,
                             height=height,
                             channels=channels,
