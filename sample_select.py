fc98b2c8a481389ce1a1abd3a8a278cfde37d1f1
hanati <hanati@tezign.com>
2019-07-19 10:47:09 +0800

    exclude hard samples

---
+++ sample_select.py
@@ -0,0 +1,23 @@
+"""
+Name : sample_select.py
+Author  : Hanat
+Contect : hanati@tezign.com
+Time    : 2019-07-19 10:41
+Desc:
+"""
+
+
+import os
+import shutil
+
+path1 = "./dataset/train_fonts99/train_hard"
+path2 = "./dataset/train_fonts99/val"
+path3 = "./dataset/train_fonts99/val_hard"
+
+class_name1 = [dir_class for dir_class in os.listdir(path1)]
+class_name2 = [os.path.join(path2, dir_class) for dir_class in os.listdir(path2)]
+
+for name in class_name2:
+    if name.split("/")[-1] in class_name1:
+        move_path = path3+"/"+name
+        shutil.move(name, move_path)
